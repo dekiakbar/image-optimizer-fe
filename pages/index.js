0b51e6983bd435e42717a73f78306f725d517085
Deki <dekiakbar@linuxmail.org>
2022-09-11 16:57:59 +0700

    update seo #5

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,13 @@ export async function getStaticProps() {
     const res = await fetch(process.env.NEXT_PUBLIC_API_CONFIG_URL)
     const config = await res.json();
     config.endpoint = process.env.NEXT_PUBLIC_API_SERVER_URL;
+    config.seo = {
+        'url': process.env.SITE_URL,
+        'title': process.env.SITE_TITLE,
+        'desc': process.env.SITE_DESC,
+        'keyword': process.env.SITE_KEYS,
+        'image': process.env.SITE_URL+'/'+process.env.SITE_IMAGE,
+    }
     return { props: { config } }
 }
 
@@ -14,9 +21,26 @@ export default function Home({ config }) {
     return (
     <div className={styles.container}>
         <Head>
-            <title>Image Optimizer</title>
-            <meta name="description" content="Optimize/compress png,jpg,jpeg image using this app" />
+            <title>{config.seo.title}</title>
             <link rel="icon" href="/favicon.ico" />
+            <link rel="canonical" href={config.seo.url} />
+            <meta name="robots" content="index,follow" />
+            <meta name="googlebot" content="index,follow" />
+            <meta name="googlebot-news" content="index,follow" />
+            <meta name="googlebot" content="index,follow" />
+            <meta name="description" content={config.seo.desc} />
+            <meta name="keywords" content={config.seo.keyword}/>
+            <meta name="image" content={config.seo.image} />
+            <meta property="og:type" content="website"/>
+            <meta property="og:url" content={config.seo.url}/>
+            <meta property="og:title" content={config.seo.title}/>
+            <meta property="og:description" content={config.seo.desc}/>
+            <meta property="og:image" content={config.seo.image}/>
+            <meta property="twitter:card" content="summary_large_image"/>
+            <meta property="twitter:url" content={config.seo.url}/>
+            <meta property="twitter:title" content={config.seo.title}/>
+            <meta property="twitter:description" content={config.seo.desc}/>
+            <meta property="twitter:image" content={config.seo.image}/>
         </Head>
 
         <main className={styles.main}>
